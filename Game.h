ad55753e5d2c4d9168d3ea031941a5c6e25668c5
Harris Crews <whcrews1998@gmail.com>
2022-08-07 13:41:10 -0400

    Added Game class declarations (.h)

---
+++ Game.h
@@ -0,0 +1,27 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <iostream>
+#include <SDL.h>
+
+class Game {
+
+    public:
+        Game();
+        ~Game();
+
+        void handleEvents();
+        void update();
+        void render();
+        void clean();
+
+        bool running();
+    private:
+        bool isRunning;
+
+        SDL_Window* window;
+        SDL_Renderer* renderer;
+
+};
+
+#endif
