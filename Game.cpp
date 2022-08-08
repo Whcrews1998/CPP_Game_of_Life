b0442584861989e638889c53a64513ba727abd12
Harris Crews <whcrews1998@gmail.com>
2022-08-07 21:11:53 -0400

    Basic implementation added to Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -2,21 +2,69 @@
 #include <iostream>
 
 Game::Game() {
- 
+    return;
 }
-Game::~Game() {
 
+Game::~Game() {
+    return;
+}
+
+void gameInit(const char* title, int xpos, int ypos, int width, int height, bool fullscreen) {
+    int flag = 0;
+    if (fullscreen) 
+        flag = SDL_WINDOW_FULLSCREEN;
+
+    if (SDL_Init(SDL_INIT_EVERYTHING)  == 0) {
+        std::cout << "SDL Initialized!" << std::endl;
+        window = SDL_CreateWindow(title, xpos, ypos, width, height, )
+
+        if (window) 
+            std::cout << "Window created successfully" << std::endl;
+        else 
+            std::cout << "Faield to create window" << std::endl;
+        
+        renderer = SDL_CreateRenderer(window, -1, 0);
+        if (renderer) 
+            SDL_SetRenderDrawColor(renderer, 255,255,255,255);
+            std::cout << "Render created successfully!" << std::endl;
+        else 
+            std::cout << "Failed to create renderer" << std::endl;
+
+        isRunning = true;
+    } else {
+        isRunning = false;
+    }
 }
 
 void Game::handleEvents() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
 
+    switch(event.type) {
+        case SDL_QUIT:
+            isRunning = false;
+            break;
+        default;
+            break;
+    }
+
+    return;
 }
+
 void Game::update() {
-
+    return;
 }
+
 void Game::render() {
+    SDL_RenderClear(renderer);
 
+    return;
 }
-void Game::clean() {
 
+void Game::clean() {
+    SDL_DestroyWindow(window);
+    SDL_DestroyRenderer(renderer);
+    SDL_Quit();
+    std::cout << "Game terminated." << std::endl;
+    return;
 }
--- Game.h
+++ Game.h
@@ -10,12 +10,13 @@ class Game {
         Game();
         ~Game();
 
+        void gameInit(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
         void handleEvents();
         void update();
         void render();
         void clean();
 
-        bool running();
+        bool running(return isRunning;);
     private:
         bool isRunning;
         SDL_Window* window;
